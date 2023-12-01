4548e1529c64e33a1a9a6f222e5362cc4a628d9d
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-12-01 20:16:02 -0300

    meu deus acaba com essa dor

--- Program.cs
+++ Program.cs
@@ -1,25 +1,19 @@
 ﻿const string FraseParaSair = "tchau";
-
 Console.WriteLine("---- Groot chat -----\n");
-
 string pergunta;
-do
-{
+do {
     Console.WriteLine("pergunta: ");
     pergunta = Console.ReadLine()!;
     string resposta = obtemrespostagroot(pergunta);
     exiberesposta(obtemrespostagroot(pergunta));
-
-} while (pergunta.Trim().ToLower() != FraseParaSair);
-
+} 
+while (pergunta.Trim().ToLower() != FraseParaSair);
 void exiberesposta(string resposta)
-
 {
     Console.ForegroundColor = ConsoleColor.Green;
     Console.WriteLine($" Resposta: {resposta}");
     Console.ResetColor();
 }
-
 string obtemrespostagroot(string pergunta)
 {
     return pergunta.Trim().ToLower()== FraseParaSair ?
